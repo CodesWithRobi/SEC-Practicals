014f1da69c023bf1bb9b0d41561bce49f338d084
CodesWithRobi <rayercodes@gmail.com>
2025-08-21 14:37:01 +0530

    implemented

--- EX-2.py
+++ EX-2.py
@@ -1,26 +1,3 @@
-# Implementation-of-Simple-Linear-Regression-Model-for-Predicting-the-Marks-Scored
-
-## AIM:
-To write a program to predict the marks scored by a student using the simple linear regression model.
-
-## Equipments Required:
-1. Hardware – PCs
-2. Anaconda – Python 3.7 Installation / Jupyter notebook
-
-## Algorithm
-1. Import the needed packages. 
-2. Assigning hours to x and scores to y.
-3. Plot the scatter plot.
-4. Use mse,rmse,mae formula to find the values.
-
-## Program:
-```py
-/*
-Program to implement the simple linear regression model for predicting the marks scored.
-Developed by: ROBISNON J
-RegisterNumber: 212223040170 
-*/
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -104,29 +81,3 @@ mae = mean_absolute_error(y_test, y_pred)
 print("Mean Absolute Error = ", mae)
 rmse = np.sqrt(mse)
 print("Root Mean Square Error = ", rmse)
-
-```
-
-## Output:
-![simple linear regression model for predicting the marks scored](sam.png)
-
-#### To Read Head and Tail Files
-
-
-#### Compare Dataset
-
-
-#### Predicted Value
-
-
-#### Graph For Training Set
-
-
-#### Graph For Testing Set
-
-
-#### Error
-
-
-## Result:
-Thus the program to implement the simple linear regression model for predicting the marks scored is written and verified using python programming.
