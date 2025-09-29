1ab5ecbfacbff2502d9884f97bf26d6143a51ee6
CodesWithRobi <rayercodes@gmail.com>
2025-09-29 13:38:14 +0530

    PseudoNoGenerator

---
+++ pseudoRandomNo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  int count, min, max;
+  printf("Enter the number of random numbers to generate: ");
+  scanf("%d", &count);
+  printf("Enter the minimum value: ");
+
+  scanf("%d", &min);
+  printf("Enter the maximum value: ");
+  scanf("%d", &max);
+  srand(time(NULL));
+  printf("Pseudorandom numbers:\n");
+  for (int i = 0; i < count; i++) {
+    int random_number = (rand() % (max - min + 1)) + min;
+    printf("%d\n", random_number);
+  }
+  return 0;
+}
